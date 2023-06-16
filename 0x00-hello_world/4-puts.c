891df8faad9c42237574056fd8e5d2b5640e46a8
git config --global user.name <nyaribovic@gmail.com>
2023-06-15 19:58:17 -0700

    0x00. C - Hello, World

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+/*
+ * File: 4-puts.c
+ /
+
+#include <stdio.h>
+
+/**
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
