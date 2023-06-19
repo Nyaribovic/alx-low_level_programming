ac5c6e95c9461de40e78c18152b959c69ccb992c
git config --global user.name <nyaribovic@gmail.com>
2023-06-19 09:33:42 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,10 +1,8 @@
 /*
  * File: 101-print_comb4.c
- * Auth: Nyaribo 
+ * Auth: Nyaribo
  */
-
 #include <stdio.h>
-
 /**
  * main - Prints all possible combinations of three different digits,
  *        in ascending order, separated by a comma followed by a space.
