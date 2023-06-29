b414681bb8cdc5fbebcb8422b3d915c0e08b1697
git config --global user.name <nyaribovic@gmail.com>
2023-06-29 06:43:41 -0700

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,5 +1,4 @@
-#include "main.h"
-#include <stdio.h> 
+#include "main.h"> 
 
 /**
   * leet - Encodes a string into 1337
@@ -12,7 +11,13 @@ char *leet(char *s)
 	int a = 0, b = 0, l = 5;
 	char r[5] = {'A', 'E', 'O', 'T', 'L'};
 	char n[5] = {'4', '3', '0', '7', '1'};
-
+       /*
+	*letters a and A should be replaced by 4
+	*Letters e and E should be replaced by 3
+        *Letters o and O should be replaced by 0
+	*Letters t and T should be replaced by 7
+	*Letters l and L should be replaced by 1
+	*/
 	while (s[a])
 	{
 		b = 0;
