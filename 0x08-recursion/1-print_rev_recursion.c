ba22f02aa55f06fd55d7b9eefa557891124e7b22
git config --global user.name <nyaribovic@gmail.com>
2023-07-05 06:09:45 -0700

    Recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -5,8 +5,10 @@
  * _print_rev_recursion - Prints a string in reverse.
  * @s: The string to be printed.
  */
-void _print_rev_recursion(char *s) {
-    if (*s == '\0') {
+void _print_rev_recursion(char *s)
+{
+    if (*s == '\0')
+    {
         return;  /* Base case: End of string*/
     }
 
