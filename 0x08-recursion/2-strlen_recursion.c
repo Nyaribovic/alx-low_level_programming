7e938d12ca12e16a5e53e5582bb438397b708512
git config --global user.name <nyaribovic@gmail.com>
2023-07-05 06:35:56 -0700

    Recursion3

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,14 +7,14 @@
  *
  * Return: The length of the string.
  */
-int _strlen_recursion(char *s)
+int _strlen_recursion(char *s) 
 {
-	int len = 0;
+    if (*s == '\0')
+    {
 
-	if (*s)
-	{
-		len++;
-		len += _strlen_recursion(s + 1);
-	}
+        return 0;  
+   
+    }
 
-	return (len);
+    return 1 + _strlen_recursion(s + 1);  
+}
--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,15 +8,18 @@
  * Return: If n > 0 - the factorial of n.
  *         If n < 0 - 1 to indicate an error.
  */
-int factorial(int n) {
-    if (n < 0) {
-        return -1;  // Error: Invalid input
+int factorial(int n)
+{
+    if (n < 0) { 
+
+        return -1; 
     }
 
     if (n == 0) {
-        return 1;  // Base case: factorial of 0 is 1
+       
+	    return 1; 
     }
 
-    return n * factorial(n - 1);  // Recursive call with n-1
+    return n * factorial(n - 1);  
 }
 
