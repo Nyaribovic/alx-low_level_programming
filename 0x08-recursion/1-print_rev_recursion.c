02e2beaf6075b27b1185ba934492b240ad1c8fba
git config --global user.name <nyaribovic@gmail.com>
2023-07-05 06:03:19 -0700

    Recursion

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -7,10 +7,10 @@
  */
 void _print_rev_recursion(char *s) {
     if (*s == '\0') {
-        return;  // Base case: End of string
+        return;  /* Base case: End of string*/
     }
 
-    _print_rev_recursion(s + 1);  // Recursive call with the next character
+    _print_rev_recursion(s + 1);  /* Recursive call with the next character */
 
-    _putchar(*(s));  // Print the current character
+    _putchar(*(s)); 
 }
