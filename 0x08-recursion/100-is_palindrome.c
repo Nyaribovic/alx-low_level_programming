c13486d40813842fba86a2fd57532112787c83a8
git config --global user.name <nyaribovic@gmail.com>
2023-07-06 06:28:21 -0700

    Recursion further

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -12,7 +12,7 @@ int str_len(char *s)
 	if (*s == '\0')
 		return (0);
 
-	return 1 + str_len(s + 1);
+	return (1 + str_len(s + 1));
 }
 
 /**
@@ -31,7 +31,7 @@ int is_palindrome_helper(char *s, int start, int end)
 	if (s[start] != s[end])
 		return (0);
 
-	return is_palindrome_helper(s, start + 1, end - 1);
+	return (is_palindrome_helper(s, start + 1, end - 1));
 }
 
 /**
@@ -47,5 +47,5 @@ int is_palindrome(char *s)
 	if (len <= 1)
 		return (1);
 
-	return is_palindrome_helper(s, 0, len - 1);
+	return (is_palindrome_helper(s, 0, len - 1));
 }
