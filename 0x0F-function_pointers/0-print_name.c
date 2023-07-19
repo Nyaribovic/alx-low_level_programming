2668710a1bd11233af35e17bd017cf4da9b04a0d
git config --global user.name <nyaribovic@gmail.com>
2023-07-19 05:02:00 -0700

    C - Function pointers1

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -10,10 +10,22 @@
  * @name: The name to be printed.
  * @f: A pointer to a function that prints a name.
  */
+void print_name_recursive(char *name, void (*f)(char *));
+void print_char(char *name, void (*f)(char *));
 void print_name(char *name, void (*f)(char *))
 {
-    while (*name) {
-        f(*name);
-        name++;
+    print_name_recursive(name, f);
+}
+
+void print_name_recursive(char *name, void (*f)(char *))
+{
+    if (*name) {
+        print_char(name, f);
+        print_name_recursive(name + 1, f);
     }
 }
+
+void print_char(char *name, void (*f)(char *))
+{
+    f(*name);
+ }
