7db149e5ef713b7b06d46c3c7362b1264e35fc66
git config --global user.name <nyaribovic@gmail.com>
2023-07-27 07:11:58 -0700

    Singly linked lists update

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,24 +1,26 @@
-#include <stdlib.h>
-#include <stddef.h>
+/*
+ * File: 1-list_len.c
+ * Auth: Bundi Victor
+ */
+
 #include "lists.h"
 
 /**
- * list_len - Returns the number of elements in a linked list_t list.
- * @h: Pointer to the head of the list.
+ * list_len - Finds the number of elements in
+ *            a linked list_t list.
+ * @h: The linked list_t list.
  *
- * Return: The number of elements in the list.
+ * Return: The number of elements in h.
  */
-
 size_t list_len(const list_t *h)
 {
-    size_t count = 0;
-    const list_t *current = h;
+	size_t elements = 0;
 
-    while (current != NULL)
-    {
-        count++;
-        current = current->next;
-    }
+	while (h)
+	{
+		elements++;
+		h = h->next;
+	}
 
-    return (count);
+	return (elements);
 }
--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,30 +1,18 @@
+/*
+ * File: 100-first.c
+ * Auth: Bundi Victor
+ */
+
 #include <stdio.h>
 
-/**
- * my_startup_function - Function to be called before main.
- * It prints the message "You're beat! and yet, you must allow,
- * I bore my house upon my back!"
- */
-void my_startup_function(void) __attribute__((constructor));
+void __attribute__((constructor)) hare(void);
 
 /**
- * my_startup_function - Function to be called before main.
- * It prints the message "You're beat! and yet, you must allow,
- * I bore my house upon my back!"
+ * hare - Prints a string before the
+ *        main function is executed.
  */
-void my_startup_function(void)
+void hare(void)
 {
-    printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
-}
-
-/**
- * main - The main function.
- * It is the entry point of the program.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    printf("This is the main function.\n");
-    return 0;
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
 }
