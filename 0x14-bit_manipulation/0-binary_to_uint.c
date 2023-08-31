e3da0a23078ba636d75c843488fcc1f4fbf8e414
nyaribovic <nyaribovic@gmail.com>
2023-08-31 10:19:29 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -4,6 +4,7 @@
  */
 
 #include "main.h"
+#include <stddef.h>
 
 /**
  * binary_to_uint - Converts a binary number to an unsigned int.
@@ -18,7 +19,7 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int mult = 1;
 	int len = 0;
 
-	if (b =='\0')
+	if (b == NULL)
 		return (0);
 
 	while (b[len])
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -3,9 +3,9 @@
 
 /*
  * File: main.h
- * AUth: Bundi Victor
- * Desc: Header files for prototypes  of all functions
- *       written in the 0x14-bit_manipulation directory.
+ * Auth: Bundi Victor
+ * Desc: Header file containing prototypes for functions
+ *       in the 0x14-bit_manipulation directory.
  */
 
 int _putchar(char c);
@@ -16,6 +16,5 @@ int set_bit(unsigned long int *n, unsigned int index);
 unsigned int binary_to_uint(const char *b);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
 void print_binary(unsigned long int n);
-
 
 #endif /* MAIN_H */
