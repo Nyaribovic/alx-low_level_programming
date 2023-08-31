20515a0f78ffe1d984336b643e8a74aa4f96879c
nyaribovic <nyaribovic@gmail.com>
2023-08-31 11:16:04 -0700

    5-flip_bits.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -1,6 +1,7 @@
 /*
  * Auth: Bundi Victor
  * File: 3-set_bit.c
+ */
 
 #include "main.h"
 
@@ -17,7 +18,7 @@ int set_bit(unsigned long int *n, unsigned int index)
 	if (index >= sizeof(unsigned long int) * 8)
 		return (-1);
 
-	*n |= (1UL << index); /*Set the bit at the given index to 1*/
+	*n |= (1UL << index); 
 
 	return (1);
 }
