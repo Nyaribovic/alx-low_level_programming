8effc2e4550ecbb2d6888eb91c4e1aae2037e4c7
nyaribovic <nyaribovic@gmail.com>
2023-08-31 11:46:46 -0700

    100-get_endianness.c

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -8,17 +8,15 @@
 /**
  * clear_bit - Sets the value of a bit at a given index to 0.
  * @n: A pointer to the number containing the bit.
- * @index: The index to set the value at - indices start at 0.
+ * @index: The index of the bit to set - indices start at 0.
  *
- * Return: If an error occurs (index out of range) - -1.
- *         Otherwise - 1.
+ * Return: 1 if successful, -1 if index is out of range.
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizeof(unsigned long int) * 8)
-		return (-1);
+    if (index >= sizeof(unsigned long int) * 8)
+        return (-1);
 
-	*n &= ~(1UL << index); 
-
-	return (1);
+    *n &= ~(1UL << index); 
+    return (1);
 }
