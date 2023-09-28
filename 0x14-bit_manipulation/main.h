b7179041219aa7bccd6dc3d09a92f7ee97da92ee
nyaribovic <nyaribovic@gmail.com>
2023-09-28 06:50:00 -0700

    update

---
+++ 0x14-bit_manipulation/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdio.h>
+
+/*
+ * File: main.h
+ * Auth: Bundi Victor
+ * Desc: Header file containing prototypes for functions
+ *       in the 0x14-bit_manipulation directory.
+ */
+
+int _putchar(char c);
+int clear_bit(unsigned long int *n, unsigned int index);
+int get_bit(unsigned long int n, unsigned int index);
+int get_endianness(void);
+int set_bit(unsigned long int *n, unsigned int index);
+unsigned long int mask = 1UL << index;
+unsigned int binary_to_uint(const char *b);
+unsigned int flip_bits(unsigned long int n, unsigned long int m);
+void print_binary(unsigned long int n);
+
+#endif /* MAIN_H */
