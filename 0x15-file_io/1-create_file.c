dbfd607a572a953a025456c09a8377e07e47de8e
nyaribovic <nyaribovic@gmail.com>
2023-10-02 07:31:06 -0700

    C - File I/O

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,41 @@
+/*
+ * File: 1-create_file.c
+ * Auth: Bundi Victor
+ */
+
+#include "main.h"
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * create_file - Creates a file with the specified permissions and writes.
+ * @filename: The name of the file to create.
+ * @text_content: A NULL-terminated string to write to the file.
+ *
+ * Return: 1 on success, -1 on failure.
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int file_descriptor, write_result;
+
+	if (filename == NULL)
+		return (-1);
+
+	file_descriptor = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
+	if (file_descriptor == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		write_result = write(file_descriptor, text_content, strlen(text_content));
+		if (write_result == -1)
+		{
+			close(file_descriptor);
+			return (-1);
+		}
+	}
+
+	close(file_descriptor);
+	return (1);
+}
